70de228b5406ae05acd28080f51116633b2429cf
unknown <osemensilas@gmail.com>
2026-04-20 11:21:50 +0100

    Initial Commit

---
+++ components/sections/HeroHome.tsx
@@ -0,0 +1,14 @@
+const HeroHome = () => {
+    return ( 
+        <>
+        <section className="hero">
+            <div className="content">
+                <h1>Push your Limits with us</h1>
+                <p>From beginner to advanced, we have the perfect program for you.</p>
+            </div>
+        </section>
+        </>
+     );
+}
+ 
+export default HeroHome;
