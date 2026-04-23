c761cc885e5553331e8216713aaaed65f71e0d51
osemensilas <osemensilas@gmail.com>
2026-04-23 22:26:22 +0100

    update Plans.tsx

--- components/sections/Plans.tsx
+++ components/sections/Plans.tsx
@@ -4,6 +4,7 @@ const Plans = () => {
         <section className="h-max w-full flex flex-col gap-5 px-4 sm:px-10 py-10 bg-foreground">
             <h3 className="text-base text-primary font-bold">PAYMENT OPTIONS</h3>
             <h2 className="text-3xl text-accent font-bold">Flexible <span className="text-primary">Payment Options</span> for Everyone.</h2>
+            <p className="text-center text-accent"><span className="text-primary font-bold">Registration Fee: ₦3,000</span> — One-time payment to get started</p>
             <div className="h-max w-full flex flex-col sm:flex-row items-center justify-between gap-5 pt-5">
                 <div className="h-max w-full sm:w-[30%] bg-background rounded-xl p-5">
                     <h3 className="text-xl text-center text-accent mb-5 font-bold">Daily Plan</h3>
@@ -30,7 +31,7 @@ const Plans = () => {
                     <button className="py-2 px-2 w-full bg-foreground rounded text-center text-accent text-base cursor-pointer" type="button" title="plan button">Get Started</button>
                 </div>
                 <div className="h-max w-full sm:w-[30%] bg-background rounded-xl p-5">
-                    <h3 className="text-xl text-center text-accent mb-5 font-bold">Daily Plan</h3>
+                    <h3 className="text-xl text-center text-accent mb-5 font-bold">Weekly Plan</h3>
                     <div className="h-max w-full flex items-end mb-5 gap-1">
                         <h3 className="text-xl text-accent font-bold">₦6000</h3>
                         <span className="text-sm text-accent/50">/week</span>
@@ -54,7 +55,7 @@ const Plans = () => {
                     <button className="py-2 px-2 w-full bg-foreground rounded text-center text-accent text-base cursor-pointer" type="button" title="plan button">Get Started</button>
                 </div>
                 <div className="choosen h-max w-full sm:w-[30%] bg-background rounded-xl p-5">
-                    <h3 className="relative z-10 text-xl text-center text-accent mb-5 font-bold">Daily Plan</h3>
+                    <h3 className="relative z-10 text-xl text-center text-accent mb-5 font-bold">Monthly Plan</h3>
                     <div className="relative z-10 h-max w-full flex items-end mb-5 gap-1">
                         <h3 className="text-xl text-accent font-bold">₦10,000</h3>
                         <span className="text-sm text-accent/50">/month</span>
