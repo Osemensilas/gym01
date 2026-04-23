048502e66327a9e2ec9b9d18ffdee343b343595c
osemensilas <osemensilas@gmail.com>
2026-04-23 22:50:43 +0100

    update ContactForm.tsx

--- components/ui/ContactForm.tsx
+++ components/ui/ContactForm.tsx
@@ -1,13 +1,13 @@
 const ContactForm = () => {
   return (
     <>
-        <form className="space-y-4 bg-backgroundLight px-0 px-5 py-5 rounded w-full h-full">
+        <form className="space-y-4 bg-backgroundLight px-0 sm:px-5 py-5 rounded w-full h-full">
             <div className="h-max w-full flex flex-col sm:flex-row gap-4">
-                <input type="text" placeholder="Full Name" className="w-full h-10 px-3 border border-grey outline-none rounded text-sm" />
-                <input type="email" placeholder="Your Email" className="w-full h-10 px-3 border border-accent/50 outline-none rounded text-sm" />
+                <input type="text" placeholder="Full Name" className="w-full h-10 px-3 border border-accent/50 outline-none rounded text-sm text-accent/50" />
+                <input type="email" placeholder="Your Email" className="w-full h-10 px-3 border border-accent/50 outline-none rounded text-sm text-accent/50" />
             </div>
             <div className="h-max w-full flex flex-col sm:flex-row gap-4">
-                <input type="tel" placeholder="Phone Number" className="w-full h-10 px-3 border border-accent/50 outline-none rounded text-sm" />
+                <input type="tel" placeholder="Phone Number" className="w-full h-10 px-3 border border-accent/50 outline-none rounded text-sm text-accent/50" />
             </div>
             <textarea placeholder="Your Message" className="min-w-full max-w-full p-3 border border-accent/50 outline-none rounded h-28 text-sm" />
             <button type="submit" className="w-full bg-primary text-accent p-2 rounded font-semibold cursor-pointer">Send Now</button>
