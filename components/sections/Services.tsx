5a723526caeed1e4fac5c9b6e7776592d4e4a244
unknown <osemensilas@gmail.com>
2026-04-23 21:57:03 +0100

    New Commit

--- components/sections/Services.tsx
+++ components/sections/Services.tsx
@@ -1,25 +1,46 @@
 'use client';
 
 import Image from "next/image";
-import { useRef } from "react";
+import { useState, useRef } from 'react';
 
 const Services = () => {
+
+    const [isAtStart, setIsAtStart] = useState(true);
+    const [isAtEnd, setIsAtEnd] = useState(false);
 
     const scrollRef = useRef<HTMLDivElement | null>(null);
 
     const scrollLeft = () => {
-        scrollRef.current!.scrollBy({
+
+        if (!scrollRef.current) return;
+
+        scrollRef.current.scrollBy({
             left: -300,
             behavior: "smooth",
         });
+        setTimeout(checkScrollPosition, 300);
     };
 
     const scrollRight = () => {
-        scrollRef.current!.scrollBy({
+
+        if (!scrollRef.current) return;
+
+        scrollRef.current.scrollBy({
             left: 300,
             behavior: "smooth",
         });
+        setTimeout(checkScrollPosition, 300);
     };
+
+    const checkScrollPosition = () => {
+        if (!scrollRef.current) return;
+
+        const { scrollLeft, scrollWidth, clientWidth } = scrollRef.current;
+
+        setIsAtStart(scrollLeft === 0);
+        setIsAtEnd(scrollLeft + clientWidth >= scrollWidth - 1);
+    };
+
     return ( 
         <>
         <section className="h-max w-full flex flex-col gap-5 px-4 sm:px-10 py-10 bg-foreground">
@@ -28,15 +49,15 @@ const Services = () => {
             <div className="h-max w-full flex flex-col sm:flex-row justify-between items-center">
                 <p className="text-accent/50 text-base">Train in a motivating environment with modern equipment and expert guidance tailored to your goals. Whether you are building strength, losing weight, or improving endurance, we’re here to help you achieve real, lasting results.</p>
                 <div className="h-max w-full sm:w-2/5 hidden sm:flex justify-end items-center gap-5">
-                    <button title="nav btn" onClick={scrollLeft} type="button" className="h-7 w-15 text-accent text-sm cursor-pointer flex items-center justify-center rounded-full bg-primary">
+                    <button title="nav btn" disabled={isAtStart} onClick={scrollLeft} type="button" className="h-7 w-15 text-accent text-sm cursor-pointer flex items-center justify-center rounded-full bg-primary">
                         <i className="fa-solid fa-chevron-left"></i>
                     </button>
-                    <button title="nav btn" onClick={scrollRight} type="button" className="h-7 w-15 text-accent text-sm cursor-pointer flex items-center justify-center rounded-full bg-primary">
+                    <button title="nav btn" disabled={isAtEnd} onClick={scrollRight} type="button" className="h-7 w-15 text-accent text-sm cursor-pointer flex items-center justify-center rounded-full bg-primary">
                         <i className="fa-solid fa-chevron-right"></i>
                     </button>
                 </div>
             </div>
-            <div ref={scrollRef} className="h-max w-full overflow-x-auto sm:overflow-hidden scroll-smooth">
+            <div ref={scrollRef} onScroll={checkScrollPosition}  className="h-max w-full overflow-x-auto sm:overflow-hidden scroll-smooth">
                 <div className="h-60 w-max flex items-center gap-5">
                     <a href="#" target="_blank" className="group dark-bg relative p-4 h-50 w-50 transform transition duration-500 hover:scale-105 flex items-end">
                         <div className="absolute top-0 left-0 h-full w-full">
@@ -89,6 +110,15 @@ const Services = () => {
                         </div>
                         <div className="w-full h-max relative z-5 flex justify-between items-center text-sm">
                             <p className="group-hover:text-accent transform transition duration-500 text-accent/50 font-bold text-sm">Fexibility & Mobility</p>
+                            <i className="fa-solid fa-up-right-from-square text-accent/50 group-hover:text-primary transform transition duration-500"></i>
+                        </div>
+                    </a>
+                    <a href="#" target="_blank" className="group dark-bg relative p-4 h-50 w-50 transform transition duration-500 hover:scale-105 flex items-end">
+                        <div className="absolute top-0 left-0 h-full w-full">
+                            <Image src="/images/service8.jpg" fill alt="card image" className="object-cover rounded-xl" />
+                        </div>
+                        <div className="w-full h-max relative z-5 flex justify-between items-center text-sm">
+                            <p className="group-hover:text-accent transform transition duration-500 text-accent/50 font-bold text-sm">Body Composition</p>
                             <i className="fa-solid fa-up-right-from-square text-accent/50 group-hover:text-primary transform transition duration-500"></i>
                         </div>
                     </a>
